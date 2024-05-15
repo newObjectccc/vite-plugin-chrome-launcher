80ed94fae694bf1a1d3fefca54d4cc1af7c9deaf
newObjectccc <897333949@qq.com>
2024-05-16 00:29:18 +0800

    chore: monorepo migration

---
+++ packages/example/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { viteChromeDevPlugin } from "vite-plugin-chrome-launcher";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: { background: resolve(__dirname, "background.js") },
+    },
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "manifest.json",
+          dest: "", // dist
+        },
+      ],
+    }),
+    viteChromeDevPlugin(),
+  ],
+});
